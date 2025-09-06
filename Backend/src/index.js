c34e7b055d4e06b7a9dba1835726054455427508
Vijender9 <viju0463@gmail.com>
2025-09-06 13:45:41 +0530

    Added start script

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+  origin: process.env.CLIENT_ORIGIN,
   credentials: true,
 }));
 app.use(express.json({ limit: "1mb" }));
